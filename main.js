a5887d09fba9ec0678f0d8f8f213e1bf7e968f9b
hari saputra <hari.saputra.dev@gmail.com>
2023-07-02 10:49:31 +0700

    add v-for from array

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 1, color: "blue" },
+        { id: 2, color: "green" },
+      ],
+      sizes: [38, 39, 40, 41, 42, 43],
+    };
+  },
+});
